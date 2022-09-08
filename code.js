86b51f615a7cf6f53d0dba270bd035c03b3d4c51
Enemini <40358115+Enemini@users.noreply.github.com>
2022-09-08 18:29:02 +0100

    committing my js code

---
+++ code.js
@@ -0,0 +1,24 @@
+var output = [];
+var series = 1;
+
+
+function fizzBuzz() {
+    while (series <= 100){
+  if (series % 3 === 0 && series % 5 === 0){
+     output.push("FizzBuzz");
+ }
+ else if (series % 3 === 0){
+    output.push("Fizz");
+ }
+ else if (series % 5 === 0){
+     output.push("Buzz");
+ }
+ 
+ else {
+      output.push(series);
+ }
+     series++;
+     console.log(output);
+}
+}
+    fizzBuzz();
